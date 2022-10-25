dc8d7bfa245efdea104feac94d5d1ffe555986ba
ralexrivero <ralexrivero@gmail.com>
2022-10-25 16:47:09 -0300

    url class and methods for hash and save

--- links/models.py
+++ links/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 from hashlib import md5
 
+class URL(models.Model):
+    full_url = models.URLField(unique=True)
+    url_hash = models.URLField(unique=True)
+    clicks = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clicked(self):
+        self.clicks += 1
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.full_url.encode()).hexdigest()[:12]
+
+        return super().save(*args, **kwargs)
+    
