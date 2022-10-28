d06bd3e073762257b394651593173f28f22726f1
ralexrivero <ralexrivero@gmail.com>
2022-10-27 23:51:58 -0300

    pycodestyle fix

--- links/schema.py
+++ links/schema.py
@@ -3,15 +3,18 @@ from graphene_django import DjangoObjectType
 
 from .models import URL
 
+
 class URLType(DjangoObjectType):
     class Meta:
         model = URL
+
 
 class Query(graphene.ObjectType):
     urls = graphene.List(URLType)
 
     def resolve_urls(self, info, **kwargs):
         return URL.objects.all()
+
 
 class CreateURL(graphene.Mutation):
     url = graphene.Field(URLType)
@@ -22,6 +25,7 @@ class CreateURL(graphene.Mutation):
     def mutate(self, info, full_url):
         url = URL(full_url=full_url)
         url.save()
+
 
 class Mutation(graphene.ObjectType):
     create_url = CreateURL.Field()
--- links/views.py
+++ links/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect
-
 from .models import URL
+
 
 def root(request, url_hash):
     url = get_object_or_404(URL, url_hash=url_hash)
