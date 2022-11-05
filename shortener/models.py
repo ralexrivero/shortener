8160d36532b2efb9b29bfbf3e41e9ef93cb8c224
ralexrivero <ralexrivero@gmail.com>
2022-11-05 13:46:58 -0300

    add Url model and methods for hashing and save

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from hashlib import md5
 
-# Create your models here.
+class Url(models.Model):
+    url_full = models.URLField(unique=True)
+    url_hash = models.URLField(unique=True)
+    visits = models.IntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def visited(self):
+        self.visits += 1
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.full_url.encode()).hexdigest()[:9]
+
+        return super().save(*args, **kwargs)
